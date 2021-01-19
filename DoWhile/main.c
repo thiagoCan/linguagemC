84e1ef75b54538a9883750542ba1bd7f5b95979c
thiagoCan <56437733+thiagoCan@users.noreply.github.com>
2021-01-19 18:43:25 -0300

    Add files via upload

---
+++ DoWhile/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+
+   int i;
+
+   do{
+    printf("Teste\n");
+    printf("digite o valor de i: ");
+    scanf("%d", &i);
+   }while(i!=0);
+
+   return 0;
+}
