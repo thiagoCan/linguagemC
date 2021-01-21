6c745dbf4b014c3ef3bd388ae44152aa03ae60e6
thiagoCan <56437733+thiagoCan@users.noreply.github.com>
2021-01-21 20:44:24 -0300

    Add files via upload

---
+++ inverter número/main.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    int a, b;
+    printf("digite a: ");
+    scanf("%d", &a);
+
+    if(a>=0){
+        do{
+            b=a%10;
+            printf("%d", b);
+            a/=10;
+        }while(a!=0);
+    }
+    else if(a<1){
+        a*=-1;
+        printf("-");
+
+        do{
+            b=a%10;
+            printf("%d", b);
+            a/=10;
+        }while(a!=0);
+    }
+
+    return 0;
+}
