06bacc44481f08d446803edecbf8eb7b0e7fdbb2
thiagoCan <56437733+thiagoCan@users.noreply.github.com>
2021-02-10 08:37:31 -0300

    Add files via upload

---
+++ função/funçãoRecursiva/main.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+
+    int fatorial(int x);
+    int numero, resultado;
+
+    printf("Digite um numero inteiro: ");
+    scanf("%d", &numero);
+
+    resultado = fatorial(numero);
+    printf("O fatorial eh %d", resultado);
+
+    return 0;
+}
+
+int fatorial(int x){
+
+    int resultado;
+
+    if(x==0) resultado=1;
+
+    else resultado=x*fatorial(x-1);
+
+    return resultado;
+}
